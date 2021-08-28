cd7ba492d4f5bbb2c4f6fb8c1cc09cd8e970cab1
nink sazerac <phanchita.tj@gmail.com>
2021-08-28 22:02:34 +0700

    Python1

--- Python1/lab01_03.py
+++
@@ -1,8 +0,0 @@
-print(" *** Summation of each digit ***")
-n=int(input("Enter a positive number : "))
-sum=0
-while(n>0):
-    dig=n%10
-    sum=sum+dig
-    n=n//10
-print("Summation of each digit = ",sum)
