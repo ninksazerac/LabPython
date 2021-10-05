79f95cada4d86ae69a627cc08fa5b5a1882f0682
nink sazerac <phanchita.tj@gmail.com>
2021-10-06 01:30:50 +0700

    ืพื้นฐานมากๆ

---
+++ Midterm/lecture.py
@@ -0,0 +1,30 @@
+#การรับค่า
+n=(input('what your name? : '))
+'''n=ประเภทตัวแปร(input(ใส่ตอนinput))'''
+#การแสดงผล
+print('%s Hello' %n )
+print("hello "+n+" how are you?")
+'''ถ้ามีตัวอักษร \' กับ \"'''
+#การวนซ้ำ
+'''
+while :
+
+for n in number: #n->กำหนดตัวแปร #number->เรากำหนดarrayแล้วให้nทำงานในนั้น
+
+for i in range(1, 11): #range->limit
+
+example->
+for i in range(1, 11):   
+    if i % 2 == 1:
+        continue
+    print(i, end = ', ')
+outputที่ได้-> 2, 4, 6, 8, 10, 
+'''
+#การกำหนดเงื่อนไข
+'''
+if เงื่อนไข:
+
+elif เงื่อนไข: การทำงานคล้ายswitch case
+
+else เงื่อนไข:
+'''
